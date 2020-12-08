122e986bd9c48ed4819230bcbf40596cf25610ec
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-12-07 20:26:00 -0800

    add BrowserRouter basename={process.env.PUBLIC_URL}

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/SignIn';
 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
